2e4883ac2d89ee3d1a4fc7528a41473ea8b941bd
Artyom Konukhov <artemkonuhov008@gmail.com>
2025-10-07 10:39:52 +0300

    Add front to allowed origins

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="ML-Practicum API")
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # tighten in production
+    allow_origins=["https://the-lab-academy.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
